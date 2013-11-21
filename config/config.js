c3a08a8e77b557f5553d91776bb01d909512cf84
Bemer <bemer@yandex-team.ru>
2013-11-21 20:04:54 +0200

    Implement async retrieving information about repositories from configuration file

--- config/config.js
+++ config/config.js
@@ -5,6 +5,4 @@ config.argv()
     .env()
     .file({ file: path.join(__dirname, 'config.json') });
 
-config.add('credentials', { type: 'file', file: path.join(__dirname, 'sources.json') });
-
 module.exports = config;
