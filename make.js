070022ca3aa0b79f912c145394161fbaa116318d
tormozz48 <tormozz48@gmail.com>
2013-11-21 17:25:30 +0200

    Create empty make.js file with included logger from bem-tools

---
+++ make.js
@@ -0,0 +1,13 @@
+var BEM = require('bem'),
+    FS = require('fs'),
+    CP = require('child_process'),
+    Q = BEM.require('q'),
+    PATH = BEM.require('./path'),
+    LOGGER = BEM.require('./logger'),
+    U = BEM.require('./util');
+
+var make = function() {
+    LOGGER.info('- data source start -');
+};
+
+make();
