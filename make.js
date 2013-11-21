e30a6288aaa3d2cc2bfbfc659b2f256704b17edb
tormozz48 <tormozz48@gmail.com>
2013-11-21 20:04:54 +0200

    Implement async retrieving information about repositories from configuration file

--- make.js
+++ make.js
@@ -6,18 +6,53 @@ var BEM = require('bem'),
     PATH = BEM.require('./path'),
     LOGGER = BEM.require('./logger'),
     U = BEM.require('./util'),
+    _ = require('underscore'),
 
     config = require('./config/config'),
+    git = require('./libs/git'),
     util = require('./libs/util');
 
 var make = function() {
     LOGGER.info('- data source start -');
     util.createContentDirectory()
     .then(function() {
-
+        return resolveRepositories();
+    })
+    .then(function() {
+        return resolveTags();
     })
 };
 
+var resolveRepositories = function() {
+    var sources = config.get('sources');
 
+    var s = [];
+
+    Object.getOwnPropertyNames(sources).forEach(function(key) {
+        sources[key].forEach(function(source) {
+            var owner = source.org || source.user,
+                repositories = source.repositories;
+
+            (owner && repositories) && repositories.forEach(function(repository) {
+                s.push(_.extend(repository, { user: owner, isPrivate: key == 'private' }))
+            })
+        });
+    });
+
+    var def = Q.defer(),
+        promises = s.map(function(item) {
+            return git.getRepository(item.user, item.name, item.isPrivate);
+        });
+
+    Q.allSettled(promises).then(function(res) {
+        def.resolve(res);
+    });
+
+    return def.promise;
+};
+
+var resolveTags = function() {
+
+};
 
 make();
