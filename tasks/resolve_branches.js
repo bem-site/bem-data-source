5b3ebebd76f7818d18d9c8b59573f9e8069b1849
tormozz48 <tormozz48@gmail.com>
2013-12-12 16:44:14 +0400

    code refactoring for module tasks/resolve_branches.js

--- tasks/resolve_branches.js
+++ tasks/resolve_branches.js
@@ -10,7 +10,8 @@ var UTIL = require('util'),
     _ = BEM.require('underscore'),
 
     //application modules
-    git = require('../libs/git');
+    git = require('../libs/git'),
+    util = require('../libs/util');
 
 /**
  * Retrieves information about repository branches and filter them according to config
@@ -32,49 +33,20 @@ var execute = function(sources) {
     var def = Q.defer();
     try {
         Q.allSettled(
-                sources.map(function(item) {
-                    return git.getRepositoryBranches(item);
-                })
+                sources.map(
+                    function(item) {
+                        return git.getRepositoryBranches(item);
+                    }
+                )
             ).then(function(res) {
                 //remove all rejected promises
-                res = res.filter(function(item) {
-                    return item.state === 'fulfilled';
-                });
-
-                res = res.map(function(item) {
-                    item = item.value;
-
-                    //return array which contains only branch names
-                    var branches = item.result.map(function(branch) {
-                        return branch.name;
-                    }),
-                    source = item.source,
-                    resultBranches = [];
-
-                    //remove tags which excluded in config
-                    //remove tags which not included in config
-                    //also exclude rule have greater priority
-                    if(source.branches) {
-                        var branchesInclude = source.branches.include,
-                            branchesExclude = source.branches.exclude;
-
-                        if(_.isArray(branchesInclude)) {
-                            resultBranches = _.intersection(branches, branchesInclude);
-                        }
-                        if(_.isArray(branchesExclude)) {
-                            resultBranches = resultBranches.filter(function(branch) {
-                                return branchesExclude.indexOf(branch) === -1;
-                            });
-                        }
+                res = util.filterAndMapFulfilledPromises(res,
+                    function(item) {
+                        item = item.value;
+                        item.source.branches = filterBranches(item.source, _.pluck(item.result, 'name'));
+                        return item.source;
                     }
-
-                    if(resultBranches.length > 0) {
-                        LOGGER.debug(UTIL.format('repository: %s branches: %s will be executed', source.name, resultBranches));
-                    }
-
-                    item.source.branches = resultBranches;
-                    return item.source;
-                });
+                );
 
                 LOGGER.info('step4: - resolveBranches end');
                 def.resolve(res);
@@ -85,6 +57,47 @@ var execute = function(sources) {
         def.reject(err);
     }
     return def.promise;
+};
+
+/**
+ * remove branches which excluded in config
+ * remove branches which not included in config
+ * also exclude rule have greater priority
+ * @param source {Object} with fields:
+ * - user {String} name of user or organization
+ * - isPrivate {Boolean} indicate if repository from private github
+ * - name - {String} name of repository
+ * - targetDir - {String} target directory
+ * - docDirs - {Array} array of string path where docs are
+ * - type - {String} type of repository. Different engines should be used for different types
+ * - tags - {Array} array of tags which should be included or excluded from make process
+ * - branches - {Object} object which holds arrays of branches which should be included or excluded from make process
+ * - url - {String} git url of repository
+ * @param branches - {Array} array of branches which are actually presented for current source
+ * @returns {Array}
+ */
+var filterBranches = function(source, branches) {
+    var result = [];
+
+    if(source.branches) {
+        var branchesInclude = source.branches.include,
+            branchesExclude = source.branches.exclude;
+
+        if(_.isArray(branchesInclude)) {
+            result = _.intersection(branches, branchesInclude);
+        }
+        if(_.isArray(branchesExclude)) {
+            result = result.filter(function(branch) {
+                return branchesExclude.indexOf(branch) === -1;
+            });
+        }
+    }
+
+    if(result.length > 0) {
+        LOGGER.debug(UTIL.format('repository: %s branches: %s will be executed', source.name, result));
+    }
+
+    return result;
 };
 
 module.exports = execute;
