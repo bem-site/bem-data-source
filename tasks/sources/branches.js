9b2f581c4d6069ecb171311ed3c4512d008eb582
Bemer <bemer@yandex-team.ru>
2014-02-03 12:47:56 +0200

    Refactoring resolve branches task

--- tasks/sources/branches.js
+++ tasks/sources/branches.js
@@ -13,46 +13,50 @@ var UTIL = require('util'),
     git = require('../../libs/git'),
     util = require('../../libs/util');
 
-/**
- * Retrieves information about repository branches and filter them according to config
- * @param sources - {Array} of objects with fields:
- * - user {String} name of user or organization
- * - isPrivate {Boolean} indicate if repository from private github
- * - name - {String} name of repository
- * - targetDir - {String} target directory
- * - docDirs - {Array} array of string path where docs are
- * - type - {String} type of repository. Different engines should be used for different types
- * - tags - {Array} array of tags which should be included or excluded from make process
- * - branches - {Object} object which holds arrays of branches which should be included or excluded from make process
- * - url - {String} git url of repository
- * @returns {defer.promise|*}
- */
-var execute = function(sources) {
-    LOGGER.info('step4: - resolveBranches start');
-    var def = Q.defer();
 
-    try {
-        Q.allSettled(
-            sources.map(function(item) {
-                return git.getRepositoryBranches(item);
-            })
-        ).then(function(res) {
-            //remove all rejected promises
-            res = util.filterAndMapFulfilledPromises(res, function(item) {
-                item = item.value;
-                item.source.branches = filterBranches(item.source, _.pluck(item.result, 'name'));
-                return item.source;
-            });
+module.exports = {
 
-            LOGGER.info('step4: - resolveBranches end');
-            def.resolve(res);
-        });
+    /**
+     * Retrieves information about repository branches and filter them according to config
+     * @param sources - {Array} of objects with fields:
+     * - user {String} name of user or organization
+     * - isPrivate {Boolean} indicate if repository from private github
+     * - name - {String} name of repository
+     * - targetDir - {String} target directory
+     * - docDirs - {Array} array of string path where docs are
+     * - type - {String} type of repository. Different engines should be used for different types
+     * - tags - {Array} array of tags which should be included or excluded from make process
+     * - branches - {Object} object which holds arrays of branches which should be included or excluded from make process
+     * - url - {String} git url of repository
+     * @returns {defer.promise|*}
+     */
+    run: function(sources) {
+        LOGGER.info('step4: - resolveBranches start');
+        var def = Q.defer();
 
-    } catch(err) {
-        LOGGER.error(err.message);
-        def.reject(err);
+        try {
+            Q.allSettled(
+                    sources.map(function(item) {
+                        return git.getRepositoryBranches(item);
+                    })
+                ).then(function(res) {
+                    //remove all rejected promises
+                    res = util.filterAndMapFulfilledPromises(res, function(item) {
+                        item = item.value;
+                        item.source.branches = filterBranches(item.source, _.pluck(item.result, 'name'));
+                        return item.source;
+                    });
+
+                    LOGGER.info('step4: - resolveBranches end');
+                    def.resolve(res);
+                });
+
+        } catch(err) {
+            LOGGER.error(err.message);
+            def.reject(err);
+        }
+        return def.promise;
     }
-    return def.promise;
 };
 
 /**
@@ -102,5 +106,3 @@ var filterBranches = function(source, branches) {
 
     return result;
 };
-
-module.exports = execute;
