cdeff29be754e916cef91659fee9357e28d27564
tormozz48 <tormozz48@gmail.com>
2014-02-03 12:07:11 +0200

    Refactoring init task.

--- tasks/init.js
+++ tasks/init.js
@@ -17,46 +17,45 @@ var UTIL = require('util'),
 
     commands = require('./cmd');
 
-/**
- * Creates content and output directories
- * If content directory doesn't link to remote git repository yet
- * then initialize git repository, retrieves ssh url of remote
- * github repository for build results and link local git repository
- * in content directory with remote github repo
- * @returns {*|then}
- */
-var execute = function() {
-    var contentDir = config.get('contentDirectory'),
-        outputDir = config.get('outputDirectory');
+module.exports = {
 
-    return Q.all([
-            util.createDirectory(contentDir),
-            util.createDirectory(outputDir)
-        ]).then(function() {
+    /**
+     * Creates content and output directories
+     * If content directory doesn't link to remote git repository yet
+     * then initialize git repository, retrieves ssh url of remote
+     * github repository for build results and link local git repository
+     * in content directory with remote github repo
+     * @returns {*|then}
+     */
+    run: function() {
+        var contentDir = config.get('contentDirectory'),
+            outputDir = config.get('outputDirectory'),
+            dataRepository = config.get("dataConfig"),
+            getUrlOfRemoteDataRepository = function() {
+                return git
+                    .getRepository({
+                        user: dataRepository.user,
+                        name: dataRepository.repo,
+                        isPrivate: dataRepository.private
+                    })
+                    .then(
+                        function(res) {
+                            return res.result.ssh_url;
+                        },
+                        function() {
+                            LOGGER.error("Data repository was not found. Application will be terminated");
+                        }
+                    );
+            };
+
+        return Q.all([ util.createDirectory(contentDir), util.createDirectory(outputDir) ]).then(function() {
             if(!U.isDirectory(PATH.resolve(contentDir, '.git'))) {
                 return commands.gitInit(contentDir)
-                    .then(function() {
-                        var dataRepository = config.get("dataConfig");
-
-                        return git.getRepository({
-                                user: dataRepository.user,
-                                name: dataRepository.repo,
-                                isPrivate: dataRepository.private
-                            })
-                            .then(
-                                function(res) {
-                                    return res.result.ssh_url;
-                                },
-                                function() {
-                                    LOGGER.error("Data repository was not found. Application will be terminated");
-                                }
-                            );
-                    })
+                    .then(getUrlOfRemoteDataRepository)
                     .then(function(remoteUrl) {
                         return commands.gitRemoteAdd(contentDir, 'origin', remoteUrl);
                     });
             }
         });
+    }
 };
-
-module.exports = execute;
