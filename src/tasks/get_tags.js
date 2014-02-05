2bddfb936ce15247e8ea3b71a2b84463e8b9d40a
tormozz48 <tormozz48@gmail.com>
2014-02-05 12:30:35 +0200

    Refactoring for get_tags module. Beautify code.

--- src/tasks/get_tags.js
+++ src/tasks/get_tags.js
@@ -15,6 +15,18 @@ var util = require('util'),
 var TAGS_ALL = 'all',
     TAGS_LAST = 'last';
 
+var MSG = {
+    INFO: {
+        START: '-- get tags start --',
+        END: '-- get tags end --'
+    },
+    ERR: {
+        TAG_NOT_PRESENT: 'Tag %s does not actually presented in tags of %s repository'
+    },
+    DEBUG: {
+        TAGS_TO_EXECUTE: 'repository: %s tags: %s will be executed'
+    }
+};
 
 module.exports = {
 
@@ -33,7 +45,7 @@ module.exports = {
      * @returns {defer.promise|*}
      */
     run: function(sources) {
-        logger.info('step3: - resolveTags start');
+        logger.info(MSG.INFO.START);
 
         return q.allSettled(
                 sources.map(function(item) {
@@ -48,30 +60,20 @@ module.exports = {
                     return item.source;
                 });
 
-                logger.info('step3: - resolveTags end');
+                logger.info(MSG.INFO.END);
                 return res;
             });
     }
 };
 
 /**
- * remove tags which excluded in config
- * remove tags which not included in config
- * left only last tag if there 'last' in config
- * left all tags if there 'all' in config
- * also exclude rule have greater priority
- * @param source - {Object} with fields:
- * - user {String} name of user or organization
- * - isPrivate {Boolean} indicate if repository from private github
- * - name - {String} name of repository
- * - targetDir - {String} target directory
- * - docDirs - {Array} array of string path where docs are
- * - type - {String} type of repository. Different engines should be used for different types
- * - tags - {Object} object which holds arrays of tags which should be included or excluded from make process
- * - branches - {Object} object which holds arrays of branches which should be included or excluded from make process
- * - url - {String} git url of repository
+ * Remove tags which excluded in config.
+ * Removes tags which not included in config
+ * Left only last tag if there 'last' in config
+ * Left all tags if there 'all' in config
+ * @param source - {Object} source object
  * @param tags - {Array} array of tags which are actually presented for current source
- * @returns {Array}
+ * @returns {Array} array of source objects with settled tags for execute
  */
 var filterTags = function(source, tags) {
     var result = [];
@@ -83,12 +85,13 @@ var filterTags = function(source, tags) {
         if(_.isArray(tagsInclude)) {
             //show errors in console log if invalid tags are presented in repositories configuration
             tagsInclude.forEach(function(tagInclude) {
-               if(tags.indexOf(tagInclude) === -1) {
-                   logger.error('Tag %s does not actually presented in tags of %s repository', tagInclude, source.name);
+               if(_.indexOf(tags, tagInclude) === -1) {
+                   logger.error(MSG.ERR.TAG_NOT_PRESENT, tagInclude, source.name);
                }
             });
 
             result = _.intersection(tags, tagsInclude);
+
         }else if(_.isString(tagsInclude)) {
             if(tagsInclude === TAGS_LAST) {
                 result = [_.last(tags.sort(util.sortTags))];
@@ -97,13 +100,13 @@ var filterTags = function(source, tags) {
             }
         }
 
-        if(_.isArray(tagsExclude) && (!source.noCache || source.noCache === 'false')) {
+        if(_.isArray(tagsExclude)) {
             result = _.difference(result, tagsExclude);
         }
     }
 
     if(result.length > 0) {
-        logger.debug('repository: %s tags: %s will be executed', source.name, result);
+        logger.debug(MSG.DEBUG.TAGS_TO_EXECUTE, source.name, result);
     }
 
     return result;
