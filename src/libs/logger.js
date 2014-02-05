543835c6c5d9231c9abcf3422e46a990853d81f7
Bemer <bemer@yandex-team.ru>
2014-02-05 16:56:41 +0200

    Refactoring logger module. Fix issue with lof dublicates

--- src/libs/logger.js
+++ src/libs/logger.js
@@ -4,22 +4,18 @@
 var intel = require('intel'),
     config = require('../config');
 
+intel.setLevel(config.get('logLevel'));
+intel.addHandler(
+    new intel.handlers.Console({
+        level: intel.VERBOSE,
+        formatter: new intel.Formatter({
+            format: '[%(date)s] %(levelname)s %(name)s: %(message)s',
+            colorize: true
+        })
+    })
+);
 
 module.exports = function(module) {
     var name = module ? module.filename.split('/').slice(-2).join('/') : '';
-
-    var logger = intel.getLogger(name);
-
-    logger.setLevel(config.get('logLevel'));
-    logger.addHandler(
-        new intel.handlers.Console({
-            level: intel.VERBOSE,
-            formatter: new intel.Formatter({
-                format: '[%(date)s] %(levelname)s %(name)s: %(message)s',
-                colorize: true
-            })
-        })
-    );
-
-    return logger;
+    return intel.getLogger(name);
 };
