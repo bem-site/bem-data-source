23b7b9211abc54cc05598b53bb2547afc1c0be25
Bemer <bemer@yandex-team.ru>
2014-05-01 12:37:40 +0400

    Move ssh retrieve code to make.js module. Remove get_repositories module

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -1,7 +1,6 @@
 module.exports = {
     init: require('./init'),
     getConfig: require('./get_config'),
-    getRepositories: require('./get_repositories'),
     getTags: require('./get_tags'),
     getBranches: require('./get_branches'),
     createTargets: require('./create_targets'),
