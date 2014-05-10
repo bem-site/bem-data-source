faabd71978fa3c65c88d021150dfbb507a5af1ba
tormozz48 <tormozz48@gmail.com>
2014-05-10 22:49:48 +0400

    Add coa module for parse command line options

---
+++ src/coa.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var logger = require('./libs').logger(module);
+
+module.exports = require('coa').Cmd()
+    .name(process.argv[1])
+    .title('Library data builder')
+    .helpful()
+    .opt()
+        .name('version').title('Show version')
+        .short('v').long('version')
+        .flag()
+        .only()
+        .act(function() {
+            var p = require('../package.json');
+            return p.name + ' ' + p.version;
+        })
+        .end()
+    .opt()
+        .name('private').title('privacy of repository')
+        .short('p').long('private')
+        .req()
+        .flag()
+        .end()
+    .opt()
+        .name('user').title('User or organization for repository')
+        .short('u').long('user')
+        .req()
+        .end()
+    .opt()
+        .name('repo').title('Name of repository')
+        .short('r').long('repo')
+        .req()
+        .end()
+    .opt()
+        .name('tags').title('Name(s) of tags')
+        .short('t').long('tags')
+        .arr()
+        .end()
+    .opt()
+        .name('branches').title('Name(s) of branches')
+        .short('b').long('branches')
+        .arr()
+        .end()
+    .act(function(opts, args) {
+        logger.info('Try to build sets for:');
+        logger.info('repository privacy: %s', opts.private);
+        logger.info('repository user or organization: %s', opts.user);
+        logger.info('repository name: %s', opts.repo);
+        logger.info('repository refs %s', opts.tags || opts.branches);
+
+        if(!opts.tags && !opts.branches) {
+            logger.error('Tags or branches have not been set');
+            return;
+        }
+
+        require('./make.js').run({
+            isPrivate: opts.private,
+            user: opts.user,
+            name: opts.repo,
+            tags: opts.tags || [],
+            branches: opts.branches || []
+        });
+    });
--- src/config.js
+++ src/config.js
@@ -1,39 +1,7 @@
 var config = require('nconf'),
     path = require('path');
 
-config.argv({
-        "v": {
-          alias: 'verbose',
-          describe: 'Logger level detail',
-          demand: true,
-          default: 'info'
-        },
-        "p": {
-          alias: 'private',
-          describe: 'Private flag for repository',
-          demand: false
-        },
-        "u": {
-          alias: 'user',
-          describe: 'User or organization for repository',
-          demand: false
-        },
-        "r": {
-          alias: 'repository',
-          describe: 'Name of repository',
-          demand: false
-        },
-        "t": {
-          alias: 'tags',
-          describe: 'Name(s) of tags for creation',
-          demand: false
-        },
-        "b": {
-          alias: 'branches',
-          describe: 'Name of branch(es) for creation',
-          demand: false
-        }
-    })
+config
     .env()
     .file({ file: path.join('config', 'config.json') });
 
