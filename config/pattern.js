7235710111ff940b2da968fc8eb6465523852a7c
tormozz48 <tormozz48@gmail.com>
2014-05-15 13:23:46 +0400

    Make specific configuration for bem-info libraries

--- config/pattern.js
+++ config/pattern.js
@@ -2,58 +2,34 @@
 
 exports.getChangelog = function() {
     return {
-        'islands-components': {
-            folder: "releases",
-            pattern: "changelog.md"
+        'bem-core': {
+            folder: '',
+            pattern: 'CHANGELOG.md'
         },
-        'islands-user': {
-            folder: "releases",
-            pattern: "changelog.md"
-        },
-        'islands-page': {
-            folder: "releases",
-            pattern: "changelog.md"
-        },
-        'islands-services': {
-            folder: "releases",
-            pattern: "changelog.md"
-        },
-        'islands-search': {
-            folder: "releases",
-            pattern: "changelog.md"
-        },
-        'islands-icons': {
-            folder: "releases",
-            pattern: "changelog.md"
+        'bem-components': {
+            folder: '',
+            pattern: 'CHANGELOG.md'
         }
     };
 };
 
 exports.getMigration = function() {
+    return {};
+};
+
+exports.getPattern = function() {
     return {
-        'islands-components': {
-            folder: "releases",
-            pattern: "MIGRATION.md"
+        'bem-bl': {
+            "data": "%s.data.json",
+            "jsdoc": "%s.js-doc.html"
         },
-        'islands-user': {
-            folder: "releases",
-            pattern: "MIGRATION.md"
+        'bem-core': {
+            "data": "%s.data.json",
+            "jsdoc": "%s.js-doc.html"
         },
-        'islands-page': {
-            folder: "releases",
-            pattern: "MIGRATION.md"
-        },
-        'islands-services': {
-            folder: "releases",
-            pattern: "MIGRATION.md"
-        },
-        'islands-search': {
-            folder: "releases",
-            pattern: "MIGRATION.md"
-        },
-        'islands-icons': {
-            folder: "releases",
-            pattern: "MIGRATION.md"
+        'bem-components': {
+            "data": "%s.data.json",
+            "jsdoc": "%s.js-doc.html"
         }
-    };
+    }
 };
