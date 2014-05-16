419bdb5b1ad0ed27b757ae22d464900adc86f52e
Bemer <bemer@yandex-team.ru>
2014-05-16 13:43:05 +0400

    Add regitry method for resolve registry of github for npm install

--- config/pattern.js
+++ config/pattern.js
@@ -31,5 +31,14 @@ exports.getPattern = function() {
             "data": "%s.data.json",
             "jsdoc": "%s.js-doc.html"
         }
-    }
+    };
+};
+
+exports.getRegistry = function() {
+    return {
+        'bem-mvc': 'https://registry.npmjs.org',
+        'bem-core': 'https://registry.npmjs.org',
+        'bem-components': 'https://registry.npmjs.org',
+        'bem-bl': 'https://registry.npmjs.org'
+    };
 };
