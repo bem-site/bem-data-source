30194d8ee8db23101dab0e256751ee2e4ebf5c2e
tormozz48 <tormozz48@gmail.com>
2014-05-17 11:02:26 +0400

    Move github host constants to src/constants.js module. Code refactoring for src/make.js module

--- src/constants.js
+++ src/constants.js
@@ -12,5 +12,9 @@ module.exports = {
     NPM_REGISTRY: {
         PRIVATE: "http://npm.yandex-team.ru",
         PUBLIC: "https://registry.npmjs.org"
+    },
+    GITHUB: {
+        PRIVATE: 'github.yandex-team.ru',
+        PUBLIC: 'github.com'
     }
 };
