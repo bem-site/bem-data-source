18bb964c8ea4966c4064df606068c7442ecebe5c
Bemer <bemer@yandex-team.ru>
2014-06-19 01:02:51 +0400

    Refactoring for patern.js file and target

--- config/pattern.js
+++ config/pattern.js
@@ -1,138 +1,141 @@
 'use strict';
 
-exports.getReadme = function() {
-    return {
-        'bem-mvc': {
+module.exports = {
+    'bem-mvc': {
+        builder: 'enb',
+        readme: {
             folder: '',
             pattern: {
                 en: 'README.md',
                 ru: 'README.ru.md'
             }
         },
-        'bem-bl': {
-            folder: '',
-            pattern: {
-                en: 'README.md',
-                ru: 'README.ru.md'
-            }
-        },
-        'bem-components': {
-            folder: '',
-            pattern: 'README.md'
-        }
-    };
-};
-
-exports.getChangelog = function() {
-    return {
-        'bem-core': {
-            folder: '',
-            pattern: 'CHANGELOG.md'
-        },
-        'bem-components': {
-            folder: '',
-            pattern: 'CHANGELOG.md'
-        },
-        'bem-mvc': {
+        changelog: {
             folder: '',
             pattern: {
                 en: 'CHANGELOG.md',
                 ru: 'CHANGELOG.ru.md'
             }
         },
-        'islands-components': {
-            folder: 'releases',
-            pattern: 'changelog.md'
-        },
-        'islands-user': {
-            folder: 'releases',
-            pattern: 'changelog.md'
-        },
-        'islands-page': {
-            folder: 'releases',
-            pattern: 'changelog.md'
-        },
-        'islands-services': {
-            folder: 'releases',
-            pattern: 'changelog.md'
-        },
-        'islands-search': {
-            folder: 'releases',
-            pattern: 'changelog.md'
-        },
-        'islands-icons': {
-            folder: 'releases',
-            pattern: 'changelog.md'
-        }
-    };
-};
-
-exports.getMigration = function() {
-    return {
-        'islands-components': {
-            folder: 'releases',
-            pattern: 'MIGRATION.md'
-        },
-        'islands-user': {
-            folder: 'releases',
-            pattern: 'MIGRATION.md'
-        },
-        'islands-page': {
-            folder: 'releases',
-            pattern: 'MIGRATION.md'
-        },
-        'islands-services': {
-            folder: 'releases',
-            pattern: 'MIGRATION.md'
-        },
-        'islands-search': {
-            folder: 'releases',
-            pattern: 'MIGRATION.md'
-        },
-        'islands-icons': {
-            folder: 'releases',
-            pattern: 'MIGRATION.md'
-        },
-        'bem-components': {
-            folder: '',
-            pattern: 'MIGRATION.md'
-        }
-    };
-};
-
-exports.getPattern = function() {
-    return {
-        'bem-bl': {
-            data: '%s.data.json',
-            jsdoc: '%s.jsdoc.html'
-        },
-        'bem-core': {
-            data: '%s.data.json',
-            jsdoc: '%s.jsdoc.html'
-        },
-        'bem-components': {
-            data: '%s.data.json',
-            jsdoc: '%s.jsdoc.html'
-        },
-        'bem-mvc': {
+        pattern: {
             data: '%s.data.json',
             jsdoc: '%s.js-doc.html'
         }
-    };
-};
-
-exports.getBuildCommand = function() {
-    return {
-        'bem-bl': "YENV=production enb make examples && enb make docs",
-        'bem-core': "YENV=production enb make examples && enb make docs",
-        'bem-components': "YENV=production enb make tests && enb make docs"
-    };
-};
-
-exports.getCopyPatterns = function() {
-    return {
-        'bem-components': ['*.docs', '*.tests'],
-        'bem-core': ['*.docs', '*.examples'],
-        'bem-bl': ['*.docs', '*.examples']
-    };
+    },
+    'bem-bl': {
+        builder: 'enb',
+        command: 'YENV=production enb make examples && enb make docs',
+        copy: ['*.docs', '*.examples'],
+        readme: {
+            folder: '',
+            pattern: {
+                en: 'README.md',
+                ru: 'README.ru.md'
+            }
+        },
+        pattern: {
+            data: '%s.data.json',
+            jsdoc: '%s.jsdoc.html'
+        }
+    },
+    'bem-components': {
+        builder: 'enb',
+        command: 'YENV=production enb make tests && enb make docs',
+        copy: ['*.docs', '*.tests'],
+        readme: {
+            folder: '',
+            pattern: 'README.md'
+        },
+        changelog: {
+            folder: '',
+            pattern: 'CHANGELOG.md'
+        },
+        migration: {
+            folder: '',
+            pattern: 'MIGRATION.md'
+        },
+        pattern: {
+            data: '%s.data.json',
+            jsdoc: '%s.jsdoc.html'
+        }
+    },
+    'bem-core': {
+        builder: 'enb',
+        command: 'YENV=production enb make examples && enb make docs',
+        copy: ['*.docs', '*.examples'],
+        changelog: {
+            folder: '',
+            pattern: 'CHANGELOG.md'
+        },
+        pattern: {
+            data: '%s.data.json',
+            jsdoc: '%s.jsdoc.html'
+        }
+    },
+    'islands-components': {
+        builder: 'bem-tools',
+        changelog: {
+            folder: 'releases',
+            pattern: 'changelog.md'
+        },
+        migration: {
+            folder: 'releases',
+            pattern: 'MIGRATION.md'
+        }
+    },
+    'islands-user': {
+        builder: 'bem-tools',
+        changelog: {
+            folder: 'releases',
+            pattern: 'changelog.md'
+        },
+        migration: {
+            folder: 'releases',
+            pattern: 'MIGRATION.md'
+        }
+    },
+    'islands-page': {
+        builder: 'bem-tools',
+        changelog: {
+            folder: 'releases',
+            pattern: 'changelog.md'
+        },
+        migration: {
+            folder: 'releases',
+            pattern: 'MIGRATION.md'
+        }
+    },
+    'islands-services': {
+        builder: 'bem-tools',
+        changelog: {
+            folder: 'releases',
+            pattern: 'changelog.md'
+        },
+        migration: {
+            folder: 'releases',
+            pattern: 'MIGRATION.md'
+        }
+    },
+    'islands-search': {
+        builder: 'bem-tools',
+        changelog: {
+            folder: 'releases',
+            pattern: 'changelog.md'
+        },
+        migration: {
+            folder: 'releases',
+            pattern: 'MIGRATION.md'
+        }
+    },
+    'islands-icons': {
+        builder: 'bem-tools',
+        changelog: {
+            folder: 'releases',
+            pattern: 'changelog.md'
+        },
+        migration: {
+            folder: 'releases',
+            pattern: 'MIGRATION.md'
+        }
+    }
 };
