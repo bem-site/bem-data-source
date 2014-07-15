6e098d1ba45026ee7f981f54a4ae2d2eafd2dc35
tormozz48 <tormozz48@gmail.com>
2014-07-15 14:53:17 +0400

    Provide new docs format for library version with title support

---
+++ config/titles.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    readme: {
+        en: 'Readme',
+        ru: 'Readme'
+    },
+    changelog: {
+        en: 'Сhangelog',
+        ru: 'История изменений'
+    },
+    migration: {
+        en: 'Migration',
+        ru: 'Миграция'
+    },
+    notes: {
+        en: 'Release Notes',
+        ru: 'Примечания к релизу'
+    }
+};
