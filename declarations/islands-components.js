f474aab3a75252f35dae43ffee2d617bea5dfc4b
Bemer <bemer@yandex-team.ru>
2014-09-04 13:22:40 +0400

    Fill libs declarations. Make refactoring for target.js module

--- declarations/islands-components.js
+++ declarations/islands-components.js
@@ -1,3 +1,23 @@
-/**
- * Created by user on 04.09.14.
- */
+module.exports = {
+    default: {
+        builder: 'enb',
+        command: 'npm run build',
+        pattern: {
+            data: '%s.data.json',
+            jsdoc: '%s.jsdoc.json'
+        },
+        copy: ['*.docs', '*.tests', '*.examples'],
+        tasks: [
+            require('../src/commands/remove-output'),
+            require('../src/commands/create-output'),
+            require('../src/commands/git-clone'),
+            require('../src/commands/git-checkout'),
+            require('../src/commands/npm-install'),
+            require('../src/commands/npm-run-deps'),
+            require('../src/commands/copy-borschik'),
+            require('../src/commands/npm-run-build'),
+            require('../src/commands/copy-sets'),
+            require('../src/commands/collect-sets')
+        ]
+    }
+};
--- declarations/islands.js
+++ declarations/islands.js
@@ -1,3 +1,3 @@
-/**
- * Created by user on 04.09.14.
- */
+module.exports = {
+    default: {}
+};
