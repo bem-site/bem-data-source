4fb9da9b037dee051a6e7071bbe3d52dbdcb1283
tormozz48 <tormozz48@gmail.com>
2014-09-04 16:19:30 +0400

    Add stub for doc replacement command

--- src/commands/replace-doc.js
+++ src/commands/replace-doc.js
@@ -1,3 +1,18 @@
-/**
- * Created by user on 04.09.14.
- */
+'use strict';
+
+var path = require('path'),
+    util = require('util'),
+
+    utility = require('../util'),
+    config = require('../config'),
+    logger = require('../logger'),
+    constants = require('../constants'),
+    commander = require('../commander');
+
+function replaceDoc(repo, version, doc, url) {
+
+}
+
+module.exports = function(repo, version, doc, url) {
+
+};
