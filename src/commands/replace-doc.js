cd919dc235827542bc6a5829560a07ee1fd0b90e
Bemer <bemer@yandex-team.ru>
2014-09-05 16:07:22 +0400

    Implement replace doc command

--- src/commands/replace-doc.js
+++ src/commands/replace-doc.js
@@ -6,23 +6,32 @@ var path = require('path'),
     vow = require('vow'),
     vowFs = require('vow-fs'),
 
+    api = require('../gh-api'),
     config = require('../config'),
     logger = require('../logger'),
     common = require('./common'),
+    utility = require('../util'),
     constants = require('../constants'),
     commander = require('../commander');
 
-function replaceDoc(repo, version, doc, url) {
+/**
+ * This method needs for check if file data.json
+ * really exists in dir output/{repo}/{version} folder
+ * @param repo - {String} name of repo (lib)
+ * @param version - {String} name of library version (branch ot tag)
+ * @returns {*}
+ */
+function checkForFileExist(repo, version) {
     var repoDir = path.join(constants.DIRECTORY.OUTPUT, repo),
         versionDir = path.join(repoDir, version),
         dataPath = path.join(versionDir, 'data.json');
 
-    vowFs.exists(repoDir)
+    return vowFs.exists(repoDir)
         .then(function(exists) {
-           if(!exists) {
-               return vow.reject('Directory %s does not exists', repoDir);
-           }
-           return vowFs.exists(versionDir);
+            if(!exists) {
+                return vow.reject('Directory %s does not exists', repoDir);
+            }
+            return vowFs.exists(versionDir);
         })
         .then(function(exists) {
             if(!exists) {
@@ -41,18 +50,61 @@ function replaceDoc(repo, version, doc, url) {
                 return vow.reject('File %s is not file', dataPath);
             }
             return vowFs.read(dataPath, 'utf-8');
-        })
-        .then(function(content) {
-            try {
-                content = JSON.parse(content);
-            }catch(err) {
-                return vow.reject('File %s can not be parsed', dataPath);
-            }
-
-            if(!content.docs || !content.docs[doc]) {
-                return vow.reject('Doc with key %s does not exists', doc);
-            }
         });
+}
+
+function replaceDoc(repo, version, doc, lang, url) {
+
+    return checkForFileExist(repo, version).then(function(content) {
+        try {
+            content = JSON.parse(content);
+        }catch(err) {
+            return vow.reject('File %s can not be parsed', dataPath);
+        }
+
+        if(!content.docs || !content.docs[doc]) {
+            return vow.reject('Doc with key %s does not exists', doc);
+        }
+
+        //parse web url to gh doc for retrieve all necessary information about repository
+        var _url = url.match(/^https?:\/\/(.+?)\/(.+?)\/(.+?)\/(tree|blob)\/(.+?)\/(.+)/);
+
+        if(!_url) {
+            return vow.reject('Invalid format of url %s', url);
+        }
+
+        return api.getContent({
+                isPrivate: _url[1].indexOf('yandex') > -1,
+                user: _url[2],
+                repo: _url[3],
+                ref:  _url[5],
+                path: _url[6]
+            })
+            .then(function(data) {
+                if(!data.res) {
+                    return vow.reject('Response by url %s is empty', _url);
+                }
+                var _doc = content.docs[doc],
+                    replace = utility.mdToHtml((new Buffer(data.res.content, 'base64')).toString());
+
+                //if lang option was not set then
+                //we should replace doc for all languages
+                Object.keys(_doc.content)
+                    .filter(function(item) {
+                        return lang ? item === lang : true;
+                    }).forEach(function(item) {
+                        _doc.content[item] = replace;
+                    });
+
+                content.docs[doc] = _doc;
+                return content;
+            }).then(function(content) {
+                var repoDir = path.join(constants.DIRECTORY.OUTPUT, repo),
+                    versionDir = path.join(repoDir, version),
+                    dataPath = path.join(versionDir, 'data.json');
+                return vowFs.write(dataPath, JSON.stringify(content, null, 4), { charset: 'utf8' });
+            });
+    });
 }
 
 module.exports = function() {
@@ -76,19 +128,24 @@ module.exports = function() {
             .req()
             .end()
         .opt()
+            .name('lang').title('Document language: ru|en|...')
+            .short('l').long('lang')
+            .end()
+        .opt()
             .name('url').title('Github url of file with replacement content')
             .short('u').long('url')
             .req()
             .end()
         .act(function(opts) {
-            logger.info(''.toUpperCase.apply('Try to replace documentation for:'));
+            logger.info('TRY TO REPLACE DOCUMENTATION FOR:');
 
             logger.info('repository name: %s', opts.repo);
             logger.info('repository version %s', opts.version);
             logger.info('documentation key %s', opts.doc);
+            logger.info('documentation language %s', opts.lang || 'all');
             logger.info('replacement documentation url %s', opts.url);
 
-            return replaceDoc(opts.repo, opts.version, opts.doc, opts.url).then(common({
+            return replaceDoc(opts.repo, opts.version, opts.doc, opts.lang, opts.url).then(common({
                 commitMessage: util.format('Replace doc %s for version %s of lib %s', opts.doc, opts.version, opts.repo),
                 successMessage: 'REPLACE DOC COMMAND HAS BEEN FINISHED SUCCESSFULLY',
                 errorMessage: 'REPLACE DOC COMMAND FAILED WITH ERROR %s'
