2f3ede7f086f2677faaa8630ab52832659fe3866
tormozz48 <tormozz48@gmail.com>
2014-09-13 12:22:05 +0400

    Add children class for target with overrided methods for using in publish command

--- src/target-publish.js
+++ src/target-publish.js
@@ -1,23 +1,22 @@
 'use strict';
 
-var Target = require('./target')
+var Target = require('./target');
 
 var TargetPublish  = function (source, ref) {
-    return this.init(source, ref, null);
+    this.init(source, ref, null);
+    this.declaration.tasks = [
+        require('./tasks/collect-sets'),
+        require('./tasks/archive')
+    ];
+    this.getContentPath = function() {
+        return process.cwd();
+    };
+    this.getOutputPath = function() {
+        return process.cwd();
+    };
+    return  this;
 };
 
 TargetPublish.prototype = Target.prototype;
-
-TargetPublish.prototype.getContentPath = function() {
-    return process.cwd();
-};
-
-TargetPublish.prototype.getOutputPath = function() {
-    return process.cwd();
-};
-
-TargetPublish.prototype.getTasks = function() {
-    return [require('./tasks/collect-sets')];
-};
 
 module.exports = TargetPublish;
