22b71749ec069bb0fa015fd351421707f7fd75a4
tormozz48 <tormozz48@gmail.com>
2014-09-16 12:16:15 +0400

    Update islands-page declaration for enb builder

--- declarations/islands-page.js
+++ declarations/islands-page.js
@@ -1,3 +1,23 @@
 module.exports = {
-    default: {}
+    default: {
+        builder: 'enb',
+        command: 'npm run build',
+        pattern: {
+            data: '%s.data.json',
+            jsdoc: '%s.jsdoc.json'
+        },
+        copy: ['*.docs', '*.examples'],
+        tasks: [
+            require('../src/tasks/remove-output'),
+            require('../src/tasks/create-output'),
+            require('../src/tasks/git-clone'),
+            require('../src/tasks/git-checkout'),
+            require('../src/tasks/npm-install'),
+            require('../src/tasks/npm-run-deps'),
+            require('../src/tasks/copy-borschik'),
+            require('../src/tasks/npm-run-build'),
+            require('../src/tasks/copy-sets'),
+            require('../src/tasks/collect-sets')
+        ]
+    }
 };
