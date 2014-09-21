ffa40f8fde5f0ef9c0df42794faa8713f1301b76
tormozz48 <tormozz48@gmail.com>
2014-09-21 17:58:26 +0400

    Refactor index controller module

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,45 +2,54 @@
 
 var path = require('path'),
 
+    _ = require('lodash'),
     vow = require('vow'),
     vowFs = require('vow-fs'),
 
     constants = require('../constants'),
+    template = require('../template');
 
-    /**
-     * Ping middleware handler
-     * @param {Object} req - request object
-     * @param {Object} res - response object
-     */
-    index = function (req, res) {
-        var result = [],
-            p = path.join(process.cwd(), constants.DIRECTORY.OUTPUT);
+function getData () {
+    var result = [],
+        p = path.join(process.cwd(), constants.DIRECTORY.OUTPUT);
 
-        return vowFs.listDir(p).then(function (libs) {
-            return vow.all(libs.map(function (item) {
-                return vowFs.isDir(path.join(p, item)).then(function (isDir) {
-                    return (isDir && ['.git', 'freeze'].indexOf(item) === -1) ?
-                        vowFs.listDir(path.join(p, item)).then(function (versions) {
-                            result.push({
-                                lib: item,
-                                versions: versions.filter(function (v) { return v !== '.DS_Store'; })
-                            });
-                        }) : vow.resolve();
-                });
-            }));
+    return vowFs.listDir(p).then(function (libs) {
+        return vow.all(libs.map(function (item) {
+            return vowFs.isDir(path.join(p, item)).then(function (isDir) {
+                return (isDir && ['.git', 'freeze'].indexOf(item) === -1) ?
+                    vowFs.listDir(path.join(p, item)).then(function (versions) {
+                        result.push({
+                            lib: item,
+                            versions: versions.filter(function (v) { return v !== '.DS_Store'; })
+                        });
+                    }) : vow.resolve();
+            });
+        }));
+    }).then(function() {
+        return result;
+    });
+}
+
+/**
+ * Ping middleware handler
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+function index (req, res) {
+    return getData()
+        .then(function (result) {
+            return template.run(_.extend({ block: 'page' }, { data: result }));
         })
-        .then(function () {
-            res.status(200).json({ fileStructure: result });
+        .then(function(html) {
+            res.end(html);
         })
-        .fail(function (err) {
-            res.status(500).send('error ' + err);
+        .fail(function(err) {
+            res.status(500).end(err);
         });
-    };
+}
 
 exports.index = index;
 exports.publish = require('./publish');
-exports.getLibraries = require('./get-libraries');
-exports.getVersions = require('./get-versions');
 exports.replaceDoc = require('./replace-doc');
 exports.remove = require('./remove');
 
