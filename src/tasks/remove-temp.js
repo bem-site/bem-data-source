92132ff527ae048bc43bb8750392bd1fc4796207
tormozz48 <tormozz48@gmail.com>
2014-09-26 12:46:24 +0400

    Add new modules for remove and create temporary directories

---
+++ src/tasks/remove-temp.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var util = require('util'),
+    logger = require('../logger'),
+    utility = require('../util');
+
+/**
+ * Remove target folder in output directory
+ * @param {Target} target for building
+ * @returns {defer.promise|*}
+ */
+module.exports = function (target) {
+    logger.debug(util.format('remove temp folder for target %s', target.getName()), module);
+    return utility.removeDir(target.getTempPath()).then(function () { return target; });
+};
