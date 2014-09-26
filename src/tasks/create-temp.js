bf16cc6d61d3c512673787448b588cc452ba1683
tormozz48 <tormozz48@gmail.com>
2014-09-26 12:46:24 +0400

    Add new modules for remove and create temporary directories

---
+++ src/tasks/create-temp.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var util = require('util'),
+    vowFs = require('vow-fs'),
+    logger = require('../logger');
+
+/**
+ * Create target folder in output directory
+ * @param {Target} target for building
+ * @returns {defer.promise|*}
+ */
+module.exports = function (target) {
+    logger.debug(util.format('create temp folder for target %s', target.getName()), module);
+    return vowFs.makeDir(target.getTempPath()).then(function () { return target; });
+};
