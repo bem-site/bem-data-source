aee8efb8cdc5c49791fa3a4b61e1cb4eea2cd6f4
tormozz48 <tormozz48@gmail.com>
2014-10-09 14:01:25 +0400

    Move ru.js rsync pattern from exclude to include section

--- declarations/islands.js
+++ declarations/islands.js
@@ -9,10 +9,10 @@ module.exports = {
         docDirs: '*.docs',
         rsync: {
             targets: ['*.examples'],
-            exclude: ['*.browser.bemhtml.js', '*.css', '*.ru.js', '*.en.js', '*.tr.js', '*.all.js',
+            exclude: ['*.browser.bemhtml.js', '*.css', '*.en.js', '*.tr.js', '*.all.js',
                 '*.pre.js', '*.json', '*.md', '*.deps.js', '*.bemdecl.js', '*.en.html', '*.tr.html', '*.ru.html'
             ],
-            include: ['_.*js', '_*.css', '_*.ie.css', '_*.ie6.css', '_*.ie7.css',
+            include: ['_.*js', '*.ru.js', '_*.css', '_*.ie.css', '_*.ie6.css', '_*.ie7.css',
                 '_*.ie8.css', '_*.ie9.css', '*.bemjson.js'
             ]
         },
