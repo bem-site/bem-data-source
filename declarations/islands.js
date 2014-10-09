c818b51b45b9258c8716b67fcc6cee74c9c89d1c
tormozz48 <tormozz48@gmail.com>
2014-10-09 14:19:58 +0400

    Modify set of file patterns for rsync

--- declarations/islands.js
+++ declarations/islands.js
@@ -9,10 +9,12 @@ module.exports = {
         docDirs: '*.docs',
         rsync: {
             targets: ['*.examples'],
-            exclude: ['*.browser.bemhtml.js', '*.css', '*.en.js', '*.tr.js', '*.all.js',
-                '*.pre.js', '*.json', '*.md', '*.deps.js', '*.bemdecl.js', '*.en.html', '*.tr.html', '*.ru.html'
+            exclude: ['*.browser.bemhtml.js', '*.css', '*.en.js', '*.tr.js',
+                '*.all.js', '*.keysets.ru.js', '*.keysets.ru.js', '*.pre.js',
+                '*.json', '*.md', '*.deps.js', '*.bemdecl.js', '*.en.html',
+                '*.tr.html', '*.ru.html', '*.lang.ru.js', '*.ru.js'
             ],
-            include: ['_.*js', '*.ru.js', '_*.css', '_*.ie.css', '_*.ie6.css', '_*.ie7.css',
+            include: ['_.*js', '_*.ru.js', '_*.css', '_*.ie.css', '_*.ie6.css', '_*.ie7.css',
                 '_*.ie8.css', '_*.ie9.css', '*.bemjson.js'
             ]
         },
