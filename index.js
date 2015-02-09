e821d636b0db9d235b5a49ca1a4b100bcb455343
tormozz48 <tormozz48@gmail.com>
2015-02-09 12:01:51 +0400

    Implement 2 new commands in API for preparing and sending data

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var logger = require('./src/logger'),
     TargetView = require('./src/targets/view'),
     TargetRemove = require('./src/targets/remove'),
     TargetReplace = require('./src/targets/replace'),
-    TargetPublish = require('./src/targets/publish');
+    TargetPublish = require('./src/targets/publish'),
+    TargetPrepare = require('./src/targets/prepare'),
+    TargetSend = require('./src/targets/send');
 
 /**
  * Publish library data to storage
@@ -24,6 +26,44 @@ exports.publish = function (version, options, isDryRun) {
     logger.setProductionMode(options.logLevel);
     options.isDryRun = isDryRun;
     var target = new TargetPublish(version, options);
+    return target.execute().fail(function () { process.exit(1); });
+};
+
+/**
+ * Prepare library data for sending storage
+ * @param {String} version - name of version (branch|tag|pr)
+ * @param {Object} options - options object with fields:
+ * - {String} logLevel - logger level (debug, info, warn, error)
+ * @param {Boolean} isDryRun - dry run flag
+ * @returns {*}
+ */
+exports.prepare = function (version, options, isDryRun) {
+    logger.setProductionMode(options.logLevel);
+    options.isDryRun = isDryRun;
+    var target = new TargetPrepare(version, options);
+    return target.execute().fail(function () { process.exit(1); });
+};
+
+/**
+ * Sends library data to storage
+ * @param {String} version - name of version (branch|tag|pr)
+ * @param {Object} options - options object with fields:
+ * - {Object} storage configuration:
+ *    - {String} namespace - storage key namespace
+ *    - {Object} get - object with host and port fields that describes
+ *      host and port configuration for read requests
+ *    - {Object} post - object with host and port fields that describes
+ *      host and port configuration for write|modify requests
+ *    - {String} auth header
+ * - {String} logLevel - logger level (debug, info, warn, error)
+ * - {Number} maxOpenFiles - number of open files which can be opened at the same time
+ * @param {Boolean} isDryRun - dry run flag
+ * @returns {*}
+ */
+exports.send = function (version, options, isDryRun) {
+    logger.setProductionMode(options.logLevel);
+    options.isDryRun = isDryRun;
+    var target = new TargetSend(version, options);
     return target.execute().fail(function () { process.exit(1); });
 };
 
