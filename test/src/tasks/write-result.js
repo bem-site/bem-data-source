8b7edcdf2295065ee56691ae858ea4ae76d79992
tormozz48 <tormozz48@gmail.com>
2015-09-14 12:30:16 +0300

    Beautify code of test of write-result module

--- test/src/tasks/write-result.js
+++ test/src/tasks/write-result.js
@@ -6,26 +6,23 @@ var path = require('path'),
     WriteResult = require('../../../src/tasks/write-result');
 
 describe('tasks/write-result', function () {
-    var t;
+    var target;
     before(function () {
         process.chdir(path.resolve(__dirname, '../../test-data'));
-        t = new Target('v1.0.0', {});
+        target = new Target('v1.0.0', {});
     });
 
-    it('should be read-deps', function (done) {
-        vow.resolve(t.createResultBase())
-            .then(function (result1) {
-                var rd = new WriteResult(t);
-                return rd.run(result1)
-                    .then(function () {
-                        return vowFs.read(path.join(process.cwd(), 'data.json'), 'utf-8')
-                    })
-                    .then(function (content) {
-                        content = JSON.parse(content);
-                        content.should.be.ok;
-                        content.should.be.instanceOf(Object);
-                        done();
-                    });
+    it('should write doc structure into data.json file', function () {
+        return vow.resolve(target.createResultBase())
+            .then(function (result) {
+                return (new WriteResult(target)).run(result)
+            })
+            .then(function () {
+                return vowFs.read(path.join(process.cwd(), 'data.json'), 'utf-8')
+            })
+            .then(function (content) {
+                content = JSON.parse(content);
+                return should.deepEqual(content, target.createResultBase());
             });
     });
 
