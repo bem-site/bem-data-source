b19e88088928f150b5c57e94711226f0607d04c9
Vladimir Grinenko <i@tadatuta.com>
2019-01-23 14:37:41 +0300

    Provide support for external config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,22 @@
 'use strict';
 
 var path = require('path'),
-    nconf = require('nconf');
+    nconf = require('nconf'),
+    envPrefix = 'BEM_DATA_SOURCE_',
+    externalConfigPath = process.env.BEM_DATA_SOURCE_CONFIG_PATH;
 
-nconf.env();
+nconf.env({
+    separator: '__',
+    // BEM_DATA_SOURCE_STORAGE__COMMON__AUTH="Basic XXXXXXXXXXX" →
+    // { storage: { common: auth: 'Basic XXXXXXXXXXX } }
+    transform: function(obj) {
+        if (obj.key.indexOf(envPrefix) === 0) {
+            obj.key = obj.key.replace(envPrefix, '').toLowerCase();
+        }
+
+        return obj;
+    }
+});
 
 nconf.add('config', {
     type: 'file',
@@ -13,6 +26,11 @@ nconf.add('config', {
 nconf.add('credentials', {
     type: 'file',
     file: path.resolve(__dirname, '..', 'config/private.json')
+});
+
+externalConfigPath && nconf.add('external', {
+    type: 'file',
+    file: externalConfigPath
 });
 
 module.exports = nconf;
