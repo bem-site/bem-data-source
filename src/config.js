b1ec73b0267141a363dbc24a0994330c9ed2f782
Vladimir Grinenko <i@tadatuta.com>
2019-01-23 13:49:17 +0300

    Simplify config

--- src/config.js
+++ src/config.js
@@ -3,21 +3,16 @@
 var path = require('path'),
     nconf = require('nconf');
 
-/**
- * Application configuration module based on nconf library
- */
-module.exports = (function () {
-    nconf.env();
+nconf.env();
 
-    nconf.add('config', {
-        type: 'file',
-        file: path.resolve(__dirname, '..', 'config/public.json')
-    });
+nconf.add('config', {
+    type: 'file',
+    file: path.resolve(__dirname, '..', 'config/public.json')
+});
 
-    nconf.add('credentials', {
-        type: 'file',
-        file: path.resolve(__dirname, '..', 'config/private.json')
-    });
+nconf.add('credentials', {
+    type: 'file',
+    file: path.resolve(__dirname, '..', 'config/private.json')
+});
 
-    return nconf;
-})();
+module.exports = nconf;
